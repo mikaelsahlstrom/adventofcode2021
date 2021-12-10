8d1839ec5de077f3d781662d6f34d3bd10bee291
Mikael Sahlström <mick.sahlstrom@gmail.com>
2021-12-10 20:31:19 +0100

    Add option to run single day and part.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,59 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 mod day1;
+
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 
 fn main()
 {
-    day1::part1();
+    let matches = App::new(NAME)
+                    .version(VERSION)
+                    .author(AUTHORS)
+                    .about(DESCRIPTION)
+                    .arg(Arg::with_name("day")
+                         .help("Which day to run.")
+                         .short("d")
+                         .long("day")
+                         .value_name("NUMBER")
+                         .takes_value(true))
+                    .arg(Arg::with_name("part")
+                         .help("Which part of the day, if any.")
+                         .short("p")
+                         .long("part")
+                         .value_name("NUMBER")
+                         .takes_value(true))
+                    .get_matches();
 
-    day1::part2();
+    let day = matches.value_of("day").unwrap_or("all");
+    let part = matches.value_of("part").unwrap_or("all");
+
+    match day
+    {
+        "1" =>
+        {
+            match part
+            {
+                "1" => day1::part1(),
+                "2" => day1::part2(),
+                "all" =>
+                {
+                    day1::part1();
+                    day1::part2();
+                },
+                _ => ()
+            }
+        },
+        "all" =>
+        {
+            day1::part1();
+            day1::part2();
+        }
+        _ => ()
+    }
 }
